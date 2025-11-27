f246d139b435df4d697fb98735dd6f9234d99152
Sreyas IT Solutions <test.sreyas@gmail.com>
2025-11-27 14:04:43 +0530

    icons added

--- carelabs-strapi/types/generated/contentTypes.d.ts
+++ carelabs-strapi/types/generated/contentTypes.d.ts
@@ -538,6 +538,12 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
           localized: true;
         };
       }>;
+    icon: Schema.Attribute.String &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'> &
       Schema.Attribute.SetPluginOptions<{
         i18n: {
